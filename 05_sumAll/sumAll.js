cdd73a3fa4b8b48d6f47c1d97328cb7a8bcbe983
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-05 10:54:27 -0400

    feat: Completed 05_sumAll
    Used math shortcut (sum of consecutive numbers) for O(1) time complexity. Checks if numbers are positive integers that are not strings.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(lowerBound, upperBound) {
+    if (!(Number.isInteger(lowerBound) && Number.isInteger(upperBound))) return 'ERROR';
+    if (!(lowerBound >= 0 && upperBound >= 0)) return 'ERROR';
+    /*sum of consecutive numbers -> multiply the mean by 
+    number of elements between them, inclusive*/
+    let mean = (lowerBound + upperBound) / 2;
+    let numberOfElementsInArithmeticSeries = (Math.abs(upperBound - lowerBound) + 1);
+    return mean*numberOfElementsInArithmeticSeries
 };
-
 // Do not edit below this line
 module.exports = sumAll;
