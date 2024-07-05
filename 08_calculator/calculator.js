6a9bb1b77b323f6fe2cd9b0371a6828cd38e2011
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-05 19:42:10 -0400

    feat: Completed 08_calculator
    Implemented all the functions. Used iteration for factorial method to avoid stack overflow.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(addend1, addend2) {
+	return addend1+addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(addendList) {
+	let total = 0;
+  for (let index = 0; index < addendList.length; index++) {
+    total = total + addendList[index];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(factorList) {
+  let total = 1;
+  for (let index = 0; index < factorList.length; index++) {
+    total = total * factorList[index];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1;
+  for (let i = number; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
