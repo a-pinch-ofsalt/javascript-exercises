da303d70b5f78685f439c48d51acaa76d1aa74bb
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-05 11:23:46 -0400

    feat: Completed 07_tempConversion
    Does math, then uses toFixed() to round to nearest tenth (which outputs a string for some reason) then converts it back to a number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const { camelCase } = require("case-anything");
+
+const convertToCelsius = function(fahrenheitTemp) {
+  return Number(((fahrenheitTemp - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return Number((celsiusTemp*(9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
