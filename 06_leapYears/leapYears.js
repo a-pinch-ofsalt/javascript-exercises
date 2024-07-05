670d221b56a4a0742ce367c9fa2e07b630273821
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-05 11:09:05 -0400

    feat: Completed 06_leapYears
    For some reason, 1600 returns undefined, even though there is an 'else' statement that returns false...???

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    /*leap year is any year divisble by 4, except for years divisible
+    by 100 that are not divislbe by 400.*/
+    if (year % 100 === 0) {
+        if (year % 400 != 0) {
+            return false;
+        }
+    }
+    else if (year % 4 === 0)
+        return true;
+    else 
+        return false;
 };
 
 // Do not edit below this line
