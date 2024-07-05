f1d34154054b14351d18b8cd2848329beeaae430
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-05 10:27:04 -0400

    feat: Completed 02_repeatString
    Implemented the function in repeatString.js. Passes all tests, and returns 'ERROR' if numberOfTimes is negative.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    if (numberOfTimes < 0) return 'ERROR';
+    let repeatedString = '';
+    for (let i = 0; i < numberOfTimes; i++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
