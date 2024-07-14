5ef7ee05b27806f7931278be71d4a4edb39799a5
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-14 09:30:13 -0500

    feat: Completed 10_fibonacci
    calls fibonacci for previous two indexes until reaching base cases of 0 and 1. Returns OOPS if fibonacciIndex is negative.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciIndex) {
+    fibonacciIndex = Number(fibonacciIndex);
+    if (fibonacciIndex < 0) return "OOPS";
+    else if (fibonacciIndex == 0) return 0;
+    else if (fibonacciIndex == 1) return 1;
+    else return fibonacci(fibonacciIndex - 1) + fibonacci(fibonacciIndex - 2);
 };
 
 // Do not edit below this line
