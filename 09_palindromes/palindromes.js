48ed56bfe7ce4939b8a46f6948e353dee4e92c79
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-14 09:10:56 -0500

    feat: Completed 09_palindromes
    Removes punctuation and whitespace, then checks if reversed string is the same string.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
 
+
+const palindromes = function (str) {
+    const noPunct = removePunctuation(str.toLowerCase());
+    const alphanumeric = noPunct.replace(/\s/g, '');
+    const reversed = Array.from(alphanumeric).reverse().join('');
+    return alphanumeric === reversed;
 };
+
+function removePunctuation(text) {
+    var punctuation = /[\.,?!]/g;
+    var newText = text.replace(punctuation, "");
+    return newText;
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
