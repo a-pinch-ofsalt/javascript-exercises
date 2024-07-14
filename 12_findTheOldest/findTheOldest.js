5a2ea457498f7f91e72aed12119006387c3a8581
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-14 13:17:44 -0500

    feat: Completed 12_findtheOldest
    Gets ages, sorts them from least to greatest, then finds the user with the last age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.map((person) => {return getAgeAtDeath(person)});
+    const sortedAges = ages.sort((age1, age2) => {return age1 > age2 ? 1 : -1;});
+    const oldestAge = ages[ages.length - 1];
+    return people.find((person) => {return getAgeAtDeath(person) === oldestAge});
 };
+
+function getAgeAtDeath(person) {
+    if (!person.yearOfDeath) return new Date().getFullYear() - person.yearOfBirth;
+    else return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
